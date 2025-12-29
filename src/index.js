8a641e2a4a53db5e2d4cf4560409fa80d3cfa881
sangam <ghub487@gmail.com>
2025-12-29 14:45:41 +0545

    just sockiet io with express js

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const http = require("http")
+const {Server} = require("socket.io")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+const port = process.env.PORT || 3000;
+const server = http.createServer(app)
+
+const io= new Server(server,{
+  cors:{
+    origin: "http://localhost:7153",
+    methods:["GET", "POST"]
+  }
+});
+
+io.on("connection",(socket)=>{
+  console.log(`connected ${socket.id}`)
+  socket.on("send_message", (data)=>{
+    io.emit("recieve_message", data)
+  })
+
+
+socket.on("disconnect", ()=>{
+  console.log(`disconnected ${socket.id}`)
+})
+})
+server.listen(port,()=>{
+  console.log(`server running on http://localhost${port}`)
+})
